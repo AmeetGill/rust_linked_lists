3159bec1fd935712a8b2cb18dbc0a6b4333ad68a
Ameetpal Singh <ameetpalsingh@gmail.com>
2020-12-27 14:47:38 +0530

    Create third.rs

---
+++ src/third.rs
@@ -0,0 +1,21 @@
+use std::rc::Rc;
+
+pub struct List<T> {
+    head: Link<T>,
+}
+
+type Link<T> = Option<Rc<Node<T>>>;
+
+struct Node<T> {
+    elem: T,
+    next: Link<T>,
+}
+
+
+impl<T> List<T>{
+    pub fn new() {
+        List {
+            head: None
+        }
+    }
+}
